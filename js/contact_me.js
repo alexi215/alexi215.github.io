2f80416bc579457be5bc5644d620eab09a27ef41
alexi215 <alexi83@gmail.com>
2014-08-06 12:10:58 -0400

    adjusted ajax call

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: ".././mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
@@ -50,7 +50,7 @@ $(function() {
                     //clear all fields
                     $('#contactForm').trigger("reset");
                 },
-            });
+            });f
         },
         filter: function() {
             return $(this).is(":visible");
